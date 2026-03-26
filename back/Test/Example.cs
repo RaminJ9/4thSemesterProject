3ec3a7c6712ac66cb6172d10f569f4d1261fd4ce
Joachim Storbank <joachimstorbank@gmail.com>
2026-03-26 11:39:49 +0100

    Added component example

---
+++ back/Test/Example.cs
@@ -0,0 +1,17 @@
+﻿using Common.Contracts;
+using Common.Models;
+using System.Composition;
+
+[Export(typeof(IMachineComponent))]
+public class Example : IMachineComponent
+{
+    public Task<Tray?> Provide(Tray tray)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Tray?> Receive(Tray tray)
+    {
+        throw new NotImplementedException();
+    }
+}
