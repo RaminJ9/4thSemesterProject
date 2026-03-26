1b4155653c47b9ff10d2006ac993c3629812bca1
Joachim Storbank <joachimstorbank@gmail.com>
2026-03-26 11:24:48 +0100

    Initial comit

---
+++ back/Common/Contracts/IMachineComponent.cs
@@ -0,0 +1,15 @@
+﻿using Common.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Contracts
+{
+    public interface IMachineComponent
+    {
+        public Task<Tray?> Provide(Tray tray);
+        public Task<Tray?> Receive(Tray tray);
+    }
+}
---
+++ back/Core/Program.cs
@@ -0,0 +1,45 @@
+using Common;
+using Common.Contracts;
+using System.Composition.Hosting;
+using System.Reflection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+
+// Program.cs
+var assemblies = Directory.EnumerateFiles(AppDomain.CurrentDomain.BaseDirectory, "back.Component.*.dll")
+    .Select(Assembly.LoadFrom)
+    .ToList();
+
+var config = new ContainerConfiguration()
+    .WithAssemblies(assemblies);
+
+using var container = config.CreateContainer();
+
+var components = container.GetExports<IMachineComponent>();
+
+builder.Services.AddSingleton<IEnumerable<IMachineComponent>>(components.ToList());
+
+
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
