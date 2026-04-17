df477b79c035b594291385c630af7a514e4f04ac
freya <freyasecherandersen@gmail.com>
2026-04-17 13:19:28 +0200

    Warehouse Class

---
+++ back/Warehouse/Warehouse.cs
@@ -0,0 +1,12 @@
+﻿using Common.Contracts;
+using Common.Models;
+using System.Composition;
+
+namespace Warehouse
+{
+    [Export(typeof(MachineComponentBase))]
+    public class Warehouse
+    {
+
+    }
+}
