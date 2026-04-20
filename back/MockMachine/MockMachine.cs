b59b67defca3046995dcb8f44c153548e39ae96d
Joachim <joachimstorbank@gmail.com>
2026-04-20 09:36:27 +0200

    added bock machine

---
+++ back/MockMachine/MockMachine.cs
@@ -0,0 +1,26 @@
+﻿using Common.Contracts;
+using Common.Models;
+using System.Composition;
+
+namespace MockMachine
+{
+    [Export(typeof(MachineComponentBase))]
+    public class MockMachine : MachineComponentBase
+    {
+        private Tray? tray;
+        public MockMachine(string guid, string name, string connectionString) : base(guid, name, connectionString) { }
+        public async override Task<Tray?> Provide(Tray tray)
+        {
+            if (tray == null) return null;
+            await Task.Delay(1000);
+            return tray;
+        }
+
+        public async override Task<Tray?> Receive(Tray tray)
+        {
+            await Task.Delay(500);
+            this.tray = tray;
+            return this.tray;
+        }
+    }
+}
