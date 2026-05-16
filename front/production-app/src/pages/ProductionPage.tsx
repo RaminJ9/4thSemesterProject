9410c86a7bd1f4972e6b4cfc08a84e202104f2aa
Amalie Holten <amalie.holten.nielsen@outlook.dk>
2026-05-16 12:24:48 +0200

    You can make an save productionline

--- front/production-app/src/pages/ProductionPage.tsx
+++ front/production-app/src/pages/ProductionPage.tsx
@@ -1,47 +1,119 @@
 import "./CSS/ProductionLine.css"
+import { useState, useEffect } from "react";
 import Header from "../components/Header";
 import MachineBox from "../components/MachineBox";
 
-import type { machine } from "../model/machine";
-import { useState } from "react";
+import useProductionPage from "../viewModel/useProductionPage";
 
 function Production() {
+  const { machines, production, saveProduction } = useProductionPage();
+
   const [productionLine, setProductionLine] = useState<string[][]>([]);
 
-  return (
+  const [selectedMachine, setSelectedMachine] = useState("");  
+  
+  useEffect(() => {
+  if (production) {
+    setProductionLine(production);
+  }
+}, [production]);
+  return (  
     <>
       <Header />
-      <h1>Production Page</h1>
+      <div>
+        <div id="production">
+          { productionLine.length === 0 ?
 
-      <div id="production">
-        { productionLine.length === 0 ?
-          <div className="ProductionIndex"></div>
-          :
-          productionLine.map( step =>
-            <div className="ProductionIndex">
-              <MachineBox name={step[0]} />
-
-              <button   
-                onClick={() => {
-                  setProductionLine(prev => [...prev, ["b"]]);
-                }}
-              >
-                +
-              </button>
-            </div>
+            <div className="ProductionIndex"></div>
+            :
             
-          )
-        }
+            productionLine.map((indexInfo, indexStep) => (
+              
+              <div key={indexStep} className="ProductionIndex">
+                <div className="box">
+                  <p>{indexStep}</p>
 
-    
-        <button   
-          onClick={() => {
-            setProductionLine(prev => [...prev, ["a"]]);
+                  {productionLine[indexStep].map( line => 
+                    <MachineBox guid={line} />
+                  )}
+                </div>
+              
+                <div>
+                  <select
+                    value={selectedMachine}
+                    onChange={(e) => {
+                      setSelectedMachine(e.target.value);
+                      setProductionLine(prev =>
+                        prev.map((step, index) =>
+                          index === indexStep
+                            ? [...step, e.target.value]
+                            : step
+                        )
+                      ); 
+                      setSelectedMachine("");
+                    }}
+                  >
+                    <option value="">+</option>
+                    {machines.map((machine) => (
+                      <option
+                        key={machine.guid}
+                        value={machine.guid}
+                      >
+                        {machine.name}
+                      </option>
+                    ))}
+                  </select>
+                  
+                  <button onClick={ () => {
+                    setProductionLine(prev =>
+                      prev.filter((_, i) => i !== indexStep)
+                    );
+                  }}>
+                    Delete
+                  </button>
+                </div>
+              
+              </div> 
+            ))
+          }
+
+          <select
+            value={selectedMachine}
+            onChange={(e) => {
+              setSelectedMachine(e.target.value);
+              setProductionLine(prev => [
+                ...prev,
+                [e.target.value]
+              ]);
+              setSelectedMachine("");
+            }}
+          >
+            <option value="">+</option>
+            {machines.map((machine) => (
+              <option
+                key={machine.guid}
+                value={machine.guid}
+              >
+                {machine.name}
+              </option>
+            ))}
+          </select>
+
+        </div>
+        <button
+          onClick={async () => {
+            await saveProduction(productionLine);
+            window.location.reload();
           }}
         >
-          +
+          Save
         </button>
-      </div>  
+      </div>
+
+
+
+      <p>{production}</p> 
+      <p>{productionLine}</p>
     </>
   );
 }
--- front/production-app/src/viewModel/useProductionPage.ts
+++ front/production-app/src/viewModel/useProductionPage.ts
@@ -1,11 +1,11 @@
 import { useEffect, useState } from "react";
-import Info from "../pages/InfoPage";
 
 // Models
 import type { machine } from "../model/machine";
 
 function useProductionPage() {
     const [machines, setMachines] = useState<machine[]>([]);
+    const [production, setProduction] = useState<string[][]>([]);
 
     async function fetchMachines(): Promise<void> {
         const res = await fetch(`http://localhost:5253/api/machine`, {
@@ -16,7 +16,35 @@ function useProductionPage() {
         setMachines(await res.json());
     }
 
-    return { machines }
+    async function fetchProduction(): Promise<void> {
+        const res = await fetch(`http://localhost:5253/api/production`, {
+            method: "GET"
+        });
+        console.log("Fetched:", res);
+        
+        const data = await res.json();
+        const guidOnly: string[][] = data.map((step: machine[]) =>
+            step.map(machine => machine.guid)
+        );
+
+        setProduction(guidOnly);
+    }
+
+    async function saveProduction(productionLine: string[][]): Promise<void> {
+        const res = await fetch("http://localhost:5253/api/production", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(productionLine)
+        });
+        console.log("Fetched:", res);
+    }
+
+    useEffect(() => {
+        fetchMachines();
+        fetchProduction();
+    }, []);
+
+    return { machines, production, saveProduction }
 }
 
 export default useProductionPage;
